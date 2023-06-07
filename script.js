1361c4f0b31e2715d2744cfd5998814df92fd6da
Rushikesh Pote <69072907+rushi5059@users.noreply.github.com>
2023-06-07 19:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function() {
+  var songLink = document.getElementById("song-link");
+  songLink.href = "https://www.youtube.com/watch?v=YOUR_YOUTUBE_LINK_HERE";
+  songLink.innerText = "Song Name";
+});
